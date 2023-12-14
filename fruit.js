92510c9da56a1924ac598575e3d94df7a44d10d2
Trevor Clarke <trevorclarke@Trevors-MBP.ht.home>
2023-12-13 20:26:56 -0500

    refactor pt 2

--- fruit.js
+++ fruit.js
@@ -5,6 +5,14 @@ class Fruit extends Entity {
     return this.polygon.at(this.position);
   }
 
+  get mass() {
+    return (this.size * this.size) / 100;
+  }
+
+  get terminalVelocity() {
+    return 40;
+  }
+
   draw() {
     push();
     fill(this.color);
@@ -12,100 +20,81 @@ class Fruit extends Entity {
     this.polygon.at(this.position).points.forEach((p) => vertex(p.x, p.y));
     endShape(CLOSE);
     pop();
-
     fill(255);
     textSize(12);
     textAlign(CENTER, CENTER);
     text(this.mass.toFixed(0), this.x, this.y);
   }
 
-  // Update the position and velocity of the object
   updatePositionAndVelocity() {
     this.velocity = this.velocity.add(Physics.gravity);
     this.wrapAround();
     this.position.add_(this.velocity);
   }
 
-  // Calculate the overlap between objects
   calculateOverlapWithObjects(allObjects) {
-    let currentOverlap;
     let objectsOverlap = [];
-
     for (let i = 0; i < 5; i++) {
-      currentOverlap = allObjects.reduce((acc, object) => {
+      let currentOverlap = new Vector(0, 0);
+      allObjects.forEach((object) => {
         const overlap = this.hitbox.calculateOverlap(object.hitbox);
         if (overlap.magnitude() > 0.1) {
           objectsOverlap.push([object, overlap]);
+          currentOverlap = currentOverlap.add(overlap);
         }
-        return acc.add(overlap);
-      }, new Vector(0, 0));
-
+      });
       if (currentOverlap.magnitude() <= 0.1) break;
-
       this.position.subtract_(currentOverlap);
-      i++;
     }
-
     return objectsOverlap;
   }
 
-  // Handle collisions with other objects
   handleCollisions(objectsOverlap, totalDisplacement) {
     for (let [object, overlap] of objectsOverlap) {
       const displacementProportion =
         overlap.magnitude() / totalDisplacement.magnitude();
-
       if (object instanceof Boundary) {
         this.handleBoundaryCollision(overlap, displacementProportion);
       } else if (object instanceof Fruit) {
-        this.handleFruitCollision(object, overlap, displacementProportion);
+        this.handleFruitCollision(object, displacementProportion);
       }
     }
   }
 
-  // Handle collisions with boundaries
   handleBoundaryCollision(overlap, displacementProportion) {
     const normal = overlap.normalize();
-    this.velocity = this.velocity
-      .reflect(normal)
-      .multiply(0.9 * displacementProportion);
-  }
-
-  // Handle collisions with fruits
-  handleFruitCollision(object, overlap, displacementProportion) {
-    const relativeVelocity = this.velocity.subtract(object.velocity);
-    const normal = this.position.subtract(object.position).normalize();
-    const velocityAlongNormal = relativeVelocity.dot(normal);
-
-    if (velocityAlongNormal > 0) return;
-
-    const impulse =
-      (2 * velocityAlongNormal * displacementProportion) /
-      (this.mass + object.mass);
-    this.velocity = this.velocity.subtract(
-      normal.multiply(impulse * object.mass)
+    this.velocity = Physics.reflectAndScaleVelocity(
+      this.velocity,
+      normal,
+      0.9 * displacementProportion
     );
-    object.velocity = object.velocity.add(normal.multiply(impulse * this.mass));
   }
 
-  // The main update function
+  handleFruitCollision(object, displacementProportion) {
+    const normal = this.position.subtract(object.position).normalize();
+    const { v1, v2 } = Physics.calculateImpulseAndVelocity(
+      this.velocity,
+      object.velocity,
+      this.mass,
+      object.mass,
+      normal,
+      displacementProportion
+    );
+    this.velocity = v1;
+    object.velocity = v2;
+  }
+
   update(boundaries, fruit) {
     this.updatePositionAndVelocity();
     let originalPosition = this.position.dup;
-
-    //TOD: Pass these in and remove itself at the beginning
     const allObjects = [...boundaries, ...fruit.filter((f) => f !== this)];
-
     const objectsOverlap = this.calculateOverlapWithObjects(allObjects);
-
     const displacement = this.position.subtract(originalPosition);
     if (displacement.magnitude() < 0.1) return;
-
     const totalDisplacement = objectsOverlap.reduce(
       (acc, [_, overlap]) => acc.add(overlap),
       new Vector(0, 0)
     );
-
     this.handleCollisions(objectsOverlap, totalDisplacement);
   }
 
@@ -119,12 +108,5 @@ class Fruit extends Entity {
     const p = this.position;
     p.y = p.y > height ? -this.size : p.y;
     p.x = p.x > width ? 0 : p.x < 0 ? width : p.x;
-  }
-
-  get mass() {
-    return (this.size * this.size) / 100;
-  }
-  get terminalVelocity() {
-    return 40;
   }
 }
--- physics.js
+++ physics.js
@@ -1,5 +1,6 @@
 class Physics {
   static gravity = new Vector(0, 9.81 / 31);
+
   static perfectlyElasticCollision(v1, m1, v2, m2) {
     const v1f = v1
       .multiply(m1 - m2)
@@ -12,5 +13,31 @@ class Physics {
       .divide(m1 + m2);
 
     return { v1: v1f, v2: v2f };
+  }
+
+  static calculateImpulseAndVelocity(
+    v1,
+    v2,
+    m1,
+    m2,
+    normal,
+    displacementProportion
+  ) {
+    const relativeVelocity = v1.subtract(v2);
+    const velocityAlongNormal = relativeVelocity.dot(normal);
+    if (velocityAlongNormal > 0) return { v1, v2 };
+
+    const impulse =
+      (2 * velocityAlongNormal * displacementProportion) / (m1 + m2);
+    const v1f = v1.subtract(normal.multiply(impulse * m2));
+    const v2f = v2.add(normal.multiply(impulse * m1));
+
+    return { v1: v1f, v2: v2f };
+  }
+
+  static reflectAndScaleVelocity(v, normal, scale) {
+    const reflectedVelocity = v.reflect(normal);
+    const scaledVelocity = reflectedVelocity.multiply(scale);
+    return scaledVelocity;
   }
 }
